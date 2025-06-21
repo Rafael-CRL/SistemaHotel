8be887c283d6f50f45c6ab638198b21cd64fb349
Ray Carvalho <raycarvalhosantos22@gmail.com>
2025-06-21 15:33:01 -0300

    FinanceiroView
    Atualização no Sistema de Gestão Hoteleira – Módulo Financeiro
    Foi adicionada ao sistema a interface gráfica FinanceiroView, responsável pela visualização e controle das funcionalidades relacionadas ao setor financeiro do hotel. Essa nova janela foi integrada ao menu principal, permitindo uma navegação mais organizada entre os módulos do sistema.
    
    Quando o usuário clica no botão "Financeiro" no menu principal, a interface FinanceiroView é exibida, enquanto o menu é temporariamente ocultado (minimizado). A opção de retornar ao menu principal foi implementada por meio de um botão "Voltar", presente na tela financeira, que restaura a visibilidade do menu sem encerrar a aplicação, mantendo a fluidez e a persistência do sistema.
    
    Essa abordagem proporciona uma experiência mais intuitiva e limpa para o usuário, evitando o acúmulo de múltiplas janelas abertas simultaneamente.

--- src/sistemahotel/view/MenuViewGUI.java
+++ src/sistemahotel/view/MenuViewGUI.java
@@ -119,9 +119,9 @@ public class MenuViewGUI extends javax.swing.JFrame {
     }//GEN-LAST:event_btnGestaoActionPerformed
 
     private void btnFinanceiroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFinanceiroActionPerformed
-        // TODO add your handling code here:
-        System.out.println("Chamando Financeiro(Ray)");
-        System.out.println("A ser implementado");
+            FinanceiroView financeiroView = new FinanceiroView(this);
+            financeiroView.setVisible(true);
+            this.setVisible(false);
     }//GEN-LAST:event_btnFinanceiroActionPerformed
 
     private void btnSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairActionPerformed
